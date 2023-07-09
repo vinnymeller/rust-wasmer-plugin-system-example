78e32ab40f045952b613e4a19dbeb0d7955e68ed
Vinny Meller <vinnymeller@gmail.com>
2023-07-08 19:44:20 -0500

    init

---
+++ crates/example-plugin/src/lib.rs
@@ -0,0 +1,4 @@
+#[no_mangle]
+pub fn add(one: i32, two: i32) -> i32 {
+    one + two
+}
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        naersk-lib = pkgs.callPackage naersk { };
+        rustVersion = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+                extensions = [ "rust-src" "rust-std" ];
+                targets = [ "wasm32-unknown-unknown" ];
+            });
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            # cargo
+            # rustc
+            # rustfmt
+            # pre-commit
+            # rustPackages.clippy
+            rustVersion
+            rust-analyzer
+            ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+      });
+}
