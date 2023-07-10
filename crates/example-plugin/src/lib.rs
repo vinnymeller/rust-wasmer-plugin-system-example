cc7915df757d26779076fcaa412baf0e418adee6
Vinny Meller <vinnymeller@gmail.com>
2023-07-10 00:18:42 -0500

    use bincode to serialize/deserialize rust structs to send back and forth as bytes to wasm

--- crates/example-plugin/src/lib.rs
+++ crates/example-plugin/src/lib.rs
@@ -1,3 +1,5 @@
+use bincode::{deserialize, serialize};
+
 #[no_mangle]
 pub fn add(one: i32, two: i32) -> i32 {
     one + two
@@ -41,6 +43,27 @@ pub fn _double_nolen(ptr: i32, len: u32) -> i32 {
         String::from_utf8_lossy(slice)
     };
     let ret = double(&value);
+    let len = ret.len() as u32;
+    unsafe {
+        std::ptr::write(1 as _, len);
+    }
+    ret.as_ptr() as _
+}
+
+// rust version
+pub fn multiply(pair: (u8, String)) -> (u8, String) {
+    let s = pair.1.repeat(pair.0 as usize);
+    let u = pair.0.wrapping_mul(s.len() as u8);
+    (u, s)
+}
+
+// the wasmified version
+#[no_mangle]
+pub fn _multiply(ptr: i32, len: u32) -> i32 {
+    let slice = unsafe { std::slice::from_raw_parts(ptr as _, len as _) };
+    let pair = deserialize(slice).expect("failed to deserialize tuple");
+    let updated = multiply(pair);
+    let ret = serialize(&updated).expect("failed to serialize tuple");
     let len = ret.len() as u32;
     unsafe {
         std::ptr::write(1 as _, len);
--- crates/example-runner/src/main.rs
+++ crates/example-runner/src/main.rs
@@ -1,3 +1,5 @@
+use bincode::{deserialize, serialize};
+use std::time::{SystemTime, UNIX_EPOCH};
 use wasmer::{imports, Instance, Memory, MemoryType, Module, Pages, Store, TypedFunction, Value};
 
 static WASM: &[u8] =
@@ -80,7 +82,7 @@ fn get_double_str_dont_know_length() {
     let bytes = s.as_bytes();
     let len = bytes.len();
     // set first 4 bytes to 0
-    memory.view(&wasm.store).write(0, &[0, 0, 0, 0]).unwrap();
+    memory.view(&wasm.store).write(1, &[0, 0, 0, 0]).unwrap();
     memory.view(&wasm.store).write(5, bytes).unwrap();
     let double = wasm.instance.exports.get_function("_double_nolen").unwrap();
     let double_ptr = match double.call(&mut wasm.store, &[Value::I32(5), Value::I32(len as i32)]) {
@@ -92,15 +94,56 @@ fn get_double_str_dont_know_length() {
     };
     // get the length written to the first 4 bytes
     let mut new_len_bytes = [0u8; 4];
-    memory.view(&wasm.store).read(1, &mut new_len_bytes).unwrap();
+    memory
+        .view(&wasm.store)
+        .read(1, &mut new_len_bytes)
+        .unwrap();
     let new_len = u32::from_ne_bytes(new_len_bytes);
     // read the string now
     let mut byte_buf = vec![0; new_len as usize];
-    memory.view(&wasm.store).read(double_ptr as _, &mut byte_buf).unwrap();
+    memory
+        .view(&wasm.store)
+        .read(double_ptr as _, &mut byte_buf)
+        .unwrap();
     let new_str = String::from_utf8_lossy(&byte_buf);
     println!("original: {}", s);
     println!("wasm str: {}", new_str);
+}
 
+fn get_multiple_with_bincode() {
+    let mut wasm = get_wasm();
+    let memory = wasm.instance.exports.get_memory("memory").unwrap();
+    memory.view(&wasm.store).write(0, &[0, 0, 0, 0]).unwrap();
+    let s = "repeated string ".to_string();
+    let now = SystemTime::now();
+    let diff = now.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    let u = ((diff.as_millis() % 10) + 1) as u8;
+    let pair = (u,s);
+    let bytes = serialize(&pair).expect("Failed to serialize tuple");
+    let len = bytes.len();
+    memory.view(&wasm.store).write(4, &bytes).unwrap();
+    let multiply = wasm.instance.exports.get_function("_multiply").unwrap();
+    let multiply_start = match multiply.call(&mut wasm.store, &[Value::I32(4), Value::I32(len as _)]) {
+        Ok(l) => l.get(0).unwrap().unwrap_i32(),
+        Err(e) => {
+            println!("error: {:?}", e);
+            return;
+        }
+    };
+    let mut new_len_bytes = [0u8; 4];
+    memory
+        .view(&wasm.store)
+        .read(0, &mut new_len_bytes)
+        .unwrap();
+    let new_len = u32::from_ne_bytes(new_len_bytes);
+    let mut byte_buf = vec![0; new_len as usize];
+    memory
+        .view(&wasm.store)
+        .read(multiply_start as _, &mut byte_buf)
+        .unwrap();
+    let new_pair: (u8, String) = deserialize(&byte_buf).expect("Failed to deserialize tuple");
+    println!("original: {:?}", pair);
+    println!("wasm str: {:?}", new_pair);
 }
 
 fn main() {
@@ -108,4 +151,5 @@ fn main() {
     run_length();
     get_double_str();
     get_double_str_dont_know_length();
+    get_multiple_with_bincode();
 }
